da5c1c07dd7cb0295fad140072248e1ff04deb53
TheIndigoPenguin <indigopenguin81@gmail.com>
2025-03-25 01:53:27 +0000

    set up workspace + lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step One
+        Console.WriteLine("Conner A.");
+        Console.WriteLine("IT-1050 - Lab 1");
+        /*Name:
+        Course Title:*/
+
+        int fnum = 81;
+        string flang = "HTML";
+        double pnum = 19;
+        bool exp = true;
+
+        Console.WriteLine("My favorite number is " + fnum);
+        Console.WriteLine("My favorite programming language is " + flang);
+        Console.WriteLine("Prior to now, in other classes I've made " + pnum + " programs total");
+        Console.WriteLine("Therefore, the statement of me having programming experience would be " + exp);
+
+        const string school = "Tri-C";
+        Console.WriteLine("I go to school at " + school);
+
+
+
+
+    }
+}
