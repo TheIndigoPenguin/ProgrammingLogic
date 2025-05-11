5068d4b546e489aa803579bfa445a3d57f768f48
TheIndigoPenguin <indigopenguin81@gmail.com>
2025-05-11 17:58:49 +0000

    Lab 8 Completed! And so is the class. Have a good Summer, professor!

--- Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1630fdab1e3da29e748e2aa993872f751c1f169d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270fd88355334705455b0465176c6ae058229cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab8/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Lab8;
+
+class Animal
+{
+
+    public virtual void animalSound()
+    {
+
+        Console.WriteLine("The animal makes a sound.");
+
+    }
+
+}
+
+class Cat : Animal
+{
+
+    public override void animalSound()
+    {
+
+        Console.WriteLine("Meow!");
+
+    }
+
+}
+
+class Dog : Animal
+{
+
+    public override void animalSound()
+    {
+
+        Console.WriteLine("Bow Wow!");
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();
+
+    }
+}
